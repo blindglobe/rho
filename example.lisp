c2deccd1047a149b7a1880054dd980c582dde51b
AJ Rossini <blindglobe@gmail.com>
2014-04-10 16:00:02 +0200

    example for making a variable of a certain type.  Unfinished, and possibly wrong.
    Signed-off-by: AJ Rossini <blindglobe@gmail.com>

--- example.lisp
+++ example.lisp
@@ -18,3 +18,41 @@
 
 (typep (ref$ df 2 1) (ref$ (data-frame-column-types df) 2)) 
 
+df
+
+
+;;; example of making a variable of a defstruct'd type.
+
+(defstruct point (x 0.0 :type float) (y 0.0 :type float))
+
+(defparameter p1 (make-point :x 2.2 :y 2.3))
+
+p1
+
+(defparameter df-2
+  (make-data-frame '(foo #(1 2 3)) 
+		   '(bar ("a" "s" "d") string) 
+		   '(baz (100 102 97) (integer 90 110))
+		   '(bzr ((make-point :x 1.0 :y 2.0)
+			  (make-point :x 2.0 :y 2.0)
+			  (make-point :x 3.0 :y 2.0)
+		     point))))
+
+
+(make-strand 'bzr
+	     #((make-point :x 1.0 :y 2.0)
+	       (make-point :x 2.0 :y 2.0)
+	       (make-point :x 3.0 :y 2.0))
+	     'point)
+
+
+
+(typep #((make-point :x 1.0 :y 2.0)
+	 (make-point :x 2.0 :y 2.0)
+	 (make-point :x 3.0 :y 2.0))
+       'vector)
+
+(typep #((make-point :x 1.0 :y 2.0)
+	 (make-point :x 2.0 :y 2.0)
+	 (make-point :x 3.0 :y 2.0))
+       '(vector point *))
