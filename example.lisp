761f737df1e6897e3fd0dfce26667efd51fd282b
AJ Rossini <blindglobe@gmail.com>
2014-04-10 16:14:11 +0200

    more experiments with adding a variable whose type is POINT.  sigh.  Not there yet.
    Signed-off-by: AJ Rossini <blindglobe@gmail.com>

--- example.lisp
+++ example.lisp
@@ -29,16 +29,43 @@ df
 
 p1
 
+(defparameter df-2a
+  (make-data-frame '(foo #(1 2 3)) 
+		   '(bar ("a" "s" "d") string) 
+		   '(baz (100 102 97) (integer 90 110))
+		   (make-strand 'bzr
+				#((make-point :x 1.0 :y 2.0)
+				  (make-point :x 2.0 :y 2.0)
+				  (make-point :x 3.0 :y 2.0)))))
+
+df-2a
+
+(pprint-data-frame df-2a)
+
+(data-frame-column-types df-2a)
+
+
 (defparameter df-2
   (make-data-frame '(foo #(1 2 3)) 
 		   '(bar ("a" "s" "d") string) 
 		   '(baz (100 102 97) (integer 90 110))
-		   '(bzr ((make-point :x 1.0 :y 2.0)
+		   '(bzr #((make-point :x 1.0 :y 2.0)
 			  (make-point :x 2.0 :y 2.0)
-			  (make-point :x 3.0 :y 2.0)
-		     point))))
+			  (make-point :x 3.0 :y 2.0))
+		     point)
+		   '(bzr2 ((make-point :x 1.0 :y 2.0)
+			   (make-point :x 2.0 :y 2.0)
+			   (make-point :x 3.0 :y 2.0))
+		     point)))
 
+;;; works
+(make-strand 'bzr
+	     #((make-point :x 1.0 :y 2.0)
+	       (make-point :x 2.0 :y 2.0)
+	       (make-point :x 3.0 :y 2.0)))
 
+;;; but above has type T when we'd like type POINT.
+;;; so we try to specify, but it currently fails.
 (make-strand 'bzr
 	     #((make-point :x 1.0 :y 2.0)
 	       (make-point :x 2.0 :y 2.0)
@@ -46,13 +73,29 @@ p1
 	     'point)
 
 
+(typep #((make-point :x 1.0 :y 2.0)
+	 (make-point :x 2.0 :y 2.0)
+	 (make-point :x 3.0 :y 2.0))
+       'vector) ;; => T
 
 (typep #((make-point :x 1.0 :y 2.0)
 	 (make-point :x 2.0 :y 2.0)
 	 (make-point :x 3.0 :y 2.0))
-       'vector)
+       'list) ;; => NIL
 
 (typep #((make-point :x 1.0 :y 2.0)
 	 (make-point :x 2.0 :y 2.0)
 	 (make-point :x 3.0 :y 2.0))
-       '(vector point *))
+       '(vector point *))  ;; => T
+
+(typep #((make-point :x 1.0 :y 2.0)
+	 (make-point :x 2.0 :y 2.0)
+	 (make-point :x 3.0 :y 2.0))
+       '(vector * 3))  ;; => T
+
+(typep #((make-point :x 1.0 :y 2.0)
+	 (make-point :x 2.0 :y 2.0)
+	 (make-point :x 3.0 :y 2.0))
+       '(vector point 3))  ;; => T
+
+
