d45934f72bce828c0e176203aaa64b9409d419ed
AJ Rossini <blindglobe@gmail.com>
2014-04-24 17:35:05 +0200

    cleaned up tests so that it compiles and works.  Now need to move example variations from examples.lisp to unittests.lisp and streamline the example.
    Signed-off-by: AJ Rossini <blindglobe@gmail.com>

--- unittests.lisp
+++ unittests.lisp
@@ -1,6 +1,6 @@
 ;;; -*- Mode:Lisp; Syntax:ANSI-Common-Lisp; Coding:utf-8 -*-
 
-;;; Time-stamp: <2014-04-24 17:05:21 tony>
+;;; Time-stamp: <2014-04-24 17:33:58 tony>
 ;;; Creation:   <2014-04-14 11:18:02 tony>
 ;;; File:       unittests.lisp
 ;;; Author:     AJ Rossini <blindglobe@gmail.com>
@@ -26,12 +26,6 @@
 
 (defstruct pointSTR (x 0.0 :type float) (y 0.0 :type float))
 
-(make-strand 'bzr1-2
-	     (vector (make-pointSTR :x 1.0 :y 2.0)
-		     (make-pointSTR :x 2.0 :y 2.0)
-		     (make-pointSTR :x 3.0 :y 2.0))
-	     'pointSTR)
-
 (defclass pointCLOS ()
   ((x :type float :initarg :x :initform 0.1)
    (y :type float :initarg :y :initform 0.2))
@@ -40,7 +34,7 @@
 
 ;;; Tests
 
-(deftest strands (rho)
+(deftest strands-user/def/type (rho)
   (let ((strand-struct-without-type
 	 (make-strand 'bzr1-1
 		      (vector (make-pointSTR :x 1.0 :y 2.0)
@@ -62,8 +56,17 @@
 		      (vector (make-instance 'pointCLOS :x 1.0 :y 1.0)
 			      (make-instance 'pointCLOS :x 2.0 :y 2.0)
 			      (make-instance 'pointCLOS :x 3.0 :y 3.0))
-		      'pointCLOS))
-	(strand-string-without-type
+		      'pointCLOS)))
+    
+    (assert-true (= (length (strand-data strand-struct-without-type))
+		    (length (strand-data strand-struct-with-type))
+		    (length (strand-data strand-clos-with-type))
+		    (length (strand-data strand-clos-with-type))
+		    3))))
+
+
+(deftest strands-basic (rho)
+  (let ((strand-string-without-type
 	 (make-strand 'bzr3-1
 		      (vector "one" "two" "three")))
 	(strand-string-with-type
@@ -77,14 +80,21 @@
 	 (make-strand 'bzr4-2
 		      (vector 100 200 300)
 		      'fixnum))
-	(strand-real-without-type
+	(strand-float-without-type
+	 (make-strand 'bzr5-1
+		      (vector 150.50d0 250.50d0 350.0d0)))
+	(strand-float-with-type
 	 (make-strand 'bzr5-1
 		      (vector 150.50d0 250.50d0 350.0d0)
 		      'float)))
     
-    (assert-true (= (length (strand-data strand-struct-without-type))
-		    (length (strand-data strand-struct-with-type))
-		    3)))
+    (assert-true (= (length (strand-data strand-string-without-type))
+		    (length (strand-data strand-string-with-type))
+		    (length (strand-data strand-int-without-type))
+		    (length (strand-data strand-int-with-type))
+		    (length (strand-data strand-float-without-type))
+		    (length (strand-data strand-float-with-type))
+		    3))))
 
 
 	
@@ -97,6 +107,20 @@
 				       (vector (make-pointSTR :x 1.0 :y 2.0)
 					       (make-pointSTR :x 2.0 :y 2.0)
 					       (make-pointSTR :x 3.0 :y 2.0)))))
+	(df-2
+	 (make-data-frame '(foo #(1 2 3)) 
+			  '(bar ("a" "s" "d") string) 
+			  '(baz (100 102 97) (integer 90 110)))))
+
+    (assert-true (typep (ref$ df-1 2 1) (ref$ (data-frame-column-types df-1) 2)))))
+
+
+;;
+;;	(assert-true (typep (ref$ df-1 2 2) (ref$ (data-frame-column-types df-1) 3)))
+;;	(assert-true (typep (ref$ df-1 1 1) (ref$ (data-frame-column-types df-1) 1)))
+;;    (assert-equal (data-frame-column-types df2) (list of proper types)
+
+
 
 #|
 	(df-2
@@ -129,20 +153,16 @@
 			     (make-pointSTR :x 2.0 :y 2.0)
 			     (make-pointSTR :x 3.0 :y 2.0))
 			    pointSTR))
+
+|#
+
+#|
+
+
+ (deftest ref$ (rho) )
+
 |#	
 
-	)
-
-
-    (assert-true (typep (ref$ df-1 2 1) (ref$ (data-frame-column-types df-1) 2)))
-
-
-;;
-;;	(assert-true (typep (ref$ df-1 2 2) (ref$ (data-frame-column-types df-1) 3)))
-;;	(assert-true (typep (ref$ df-1 1 1) (ref$ (data-frame-column-types df-1) 1)))
-;;    (assert-equal (data-frame-column-types df2) (list of proper types)
-
-    ))
 
 
 ;;; Running tests
@@ -154,11 +174,20 @@
 	   :use-debugger NIL
 	   :report-progress T)
 
-(run-test 'strands
-	  :use-debugger NIL
+(run-test 'strands-user/def/type
+	  :use-debugger T
+	  :report-progress T)
+
+(run-test 'strands-basic
+	  :use-debugger T
+	  :report-progress T)
+
+(run-test 'dataframes
+	  :use-debugger T
 	  :report-progress T)
 
 
 (run-suite 'rho :report-progress nil)
 (rerun-failed-tests :use-debugger NIL)
 
+;;; End of File
