33a81f6e6795612d07422f789ea373218f481dd2
AJ Rossini <blindglobe@gmail.com>
2014-05-20 13:04:35 +0200

    moved data structure overview from example to README
    Signed-off-by: AJ Rossini <blindglobe@gmail.com>

--- example.lisp
+++ example.lisp
@@ -2,23 +2,11 @@
 
 (in-package :rho-user)
 
-;; strands are named/(optionally typed) columns of data elements.  The
-;; optional typing means that from a CL perspective, we use T for
-;; general columns, and a more specific type (either user specified or
-;; computed using 
 
 
 
 
 
-;; a DATA-FRAME is a VECTOR of STRANDS (or VECTORS).
-;; our example uses the following types:
-;; general
-;; string
-;; restricted integer range
-;; DEFSTRUCT'd simple point class
-;; DEFCLASS'd simple point class
-;;
 
 
 (defparameter df (make-data-frame '(foo #(1 2 3)) 
